6e9b631d690cc7b95c5a521b2860ce56f4fdb1d0
Terezija Umiljanovic <terezija.umiljanovic@gmail.com>
2021-09-19 04:16:12 +0200

    Header and footer setup

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <title>www.zaposlise.net</title>
+    <link rel='stylesheet' href='styles.css'/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <div class="logo">
+        <a href="/">Zaposlise.net</a>
+      </div>
+      <div class="nav">
+        <a class="nav-item" href="/">Oglasi</a>
+        <a class="nav-item" href="/">Radnik</a>
+        <a class="nav-item" href="/">Poslodavac</a>
+      </div>
+      <span href="/" class='menu-button' style="cursor:pointer">&#9776;</span>
+    </header>
+
+    <div class="page-content">
+
+    </div>
+
+    <footer>
+      <div class='footer-info'>
+        <a class="footer-logo" href="/">Zaposlise.net</a>
+        <div class='footer-info-links'>
+           <a aria-label="Pritisnuti za više informacija o nama" href="/">O nama</a>
+           <a aria-label="Pritisnuti za kontaktiranje" href="/">Kontakt</a></li>
+           <a aria-label="Pritisnuti za pomoć" href="#">Pomoć</a>
+         </ul>
+        </div>
+     </div>
+
+     <div class="footer-social-privacy">
+       <div class="footer-social">
+         <a href='/'>
+           <img src='images/icon-instagram.svg' aria-label="Pritisnuti za
+           otvaranje Instagram profila"
+           alt='Instagram logo' loading = 'lazy'>
+         </a>
+         <a href='/'>
+           <img src='images/icon-twitter.svg' alt='Twitter logo'
+           aria-label="Pritisnuti za otvaranje Twitter profila"
+           loading = 'lazy'>
+         </a>
+         <a href='/'>
+           <img src='images/icon-facebook.svg' alt='Facebook logo'
+           aria-label="Pritisnuti za otvaranje Facebook profila"
+           loading = 'lazy'>
+         </a>
+       </div>
+
+       <div class="footer-privacy">
+         <a aria-label="Pritisnuti za otvaranje police privatnosti" href='/'>Polica privatnosti</a>
+         <a aria-label="Pritisnuti za otvaranje uvjeta korištenja" href='/'>Uvjeti korištenja</a></li>
+         <a href='/'>@ 2021 zaposlise.net</a>
+       </div>
+     </div>
+    </footer>
+
+    <script type="text/javascript">
+
+    </script>
+  </body>
+</html>
