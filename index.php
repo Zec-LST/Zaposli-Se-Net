23c23411a5dd63ef340711d614295230f0c46195
Terezija Umiljanovic <37843034+TeaUmily@users.noreply.github.com>
2021-09-20 13:07:29 +0200

    Landing page frontend done (#4)

--- index.php
+++ index.php
@@ -19,16 +19,87 @@
         <a href="/">Zaposlise.net</a>
       </div>
       <div class="nav">
-        <a class="nav-item" href="/">Oglasi</a>
+        <a class="nav-item" href="#ads">Oglasi</a>
         <a class="nav-item" href="/">Radnik</a>
-        <a class="nav-item" href="/">Poslodavac</a>
+        <div class="dropdown">
+          <button class="dropbtn">Poslodavac</button>
+          <div class="dropdown-content">
+            <a href="/">Prijava</a>
+            <a href="/">Registracija</a>
+            <a href="/">Forum</a>
+          </div>
+        </div>
       </div>
       <span href="/" class='menu-button' style="cursor:pointer">&#9776;</span>
     </header>
 
-    <div class="page-content">
+    <main class="page-content">
+      <div class="landing">
+        <div class="landing-text">
+          <p class="landing-title">Više od 1000 oglasa na jednom mjestu</p>
+          <p class="landing-subtitle">Gradimo bolju budućnost zajedno i za svakog!</p>
+          <a class="primary-button" href="#ads">Pregledaj oglase</a>
+        </div>
+        <img class="landing-image" src="images/header-image.png" alt="">
+      </div>
 
-    </div>
+      <div class="main-section">
+        <div class="side-section">
+
+          <div class="subscription-section">
+            <p class="subscription-title">Želiš li među prvima saznati za nove oglase?</p>
+            <p class="subscription-subtitle">Upiši svoju email adresu i pretplati se za naš Newsletter</p>
+            <div class="email-input-container">
+              <img class="email-icon" src="images/mail-icon.svg" alt="">
+              <input class="email-input" type="email" name="email" placeholder="Email address">
+            </div>
+            <a class="subscription-button" href="/">Pretplati me</a>
+          </div>
+
+          <div class="advertising-space">
+            <p>Mjesto za reklamu</p>
+          </div>
+
+        </div>
+        <div id="ads" class="ads">
+
+          <div class="ads-section-header">
+            <h2>Oglasi</h2>
+            <div class="filters">
+              <div class="filter">
+                <label for="county">Županija</label>
+                <select name="county" id="county">
+                  <option value="volvo">Sve</option>
+                  <option value="volvo">Osječko-baranjska</option>
+                  <option value="saab">Zagrebačka</option>
+                  <option value="mercedes">Požeško-slavonska</option>
+                  <option value="audi">Dubrovačka</option>
+                </select>
+              </div>
+              <div class="filter">
+                <label for="category">Kategorija posla</label>
+                <select name="category" id="county">
+                  <option value="volvo">Sve</option>
+                  <option value="volvo">Studentski posao</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="jobs-list">
+            <div class="job-item">
+              <img class="ad-item-img" src="" alt="">
+              <div class="place-and-category">
+                  <p class="job-place">Osijek</p>
+                  <div class="job-category">Studentski posao</div>
+              </div>
+              <p class="job-title">Prodajni predstavnik</p>
+              <p class="job-date">Prijave do 30.rujna 2021.</p>
+            </div>
+        </div>
+          <a class="show-all-button" href="/">Pogledaj sve ></a>
+      </div>
+    </main>
 
     <footer>
       <div class='footer-info'>
@@ -43,17 +114,17 @@
 
      <div class="footer-social-privacy">
        <div class="footer-social">
-         <a href='/'>
+         <a href='https://www.instagram.com/'>
            <img src='images/icon-instagram.svg' aria-label="Pritisnuti za
            otvaranje Instagram profila"
            alt='Instagram logo' loading = 'lazy'>
          </a>
-         <a href='/'>
+         <a href='https://twitter.com/'>
            <img src='images/icon-twitter.svg' alt='Twitter logo'
            aria-label="Pritisnuti za otvaranje Twitter profila"
            loading = 'lazy'>
          </a>
-         <a href='/'>
+         <a href='https://www.facebook.com/'>
            <img src='images/icon-facebook.svg' alt='Facebook logo'
            aria-label="Pritisnuti za otvaranje Facebook profila"
            loading = 'lazy'>
