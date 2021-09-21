e14b63db878e62cea7495c0e74ebd1f105473a26
Terezija Umiljanovic <terezija.umiljanovic@gmail.com>
2021-09-21 23:59:40 +0200

    DBconfig changed

--- database/dbconfig.php
+++ database/dbconfig.php
@@ -2,14 +2,14 @@
 
 final class DBConfig
 {
-    //const HOST = 'sql4.freesqldatabase.com';
-   // const DB_NAME = 'sql4438613';
-   // const USERNAME = 'sql4438613';
-    //const PASSWORD = 'nMQd5n8dE4';
+    const HOST = 'sql4.freesqldatabase.com';
+    const DB_NAME = 'sql4438613';
+    const USERNAME = 'sql4438613';
+    const PASSWORD = 'nMQd5n8dE4';
 
 
-    const HOST = 'db';
+  /*  const HOST = 'db';
     const DB_NAME = 'ferit';
     const USERNAME = 'user';
-    const PASSWORD = 'test';
+    const PASSWORD = 'test';*/
 }
